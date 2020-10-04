59b67138f4ab725fbed6b5c6e952397db25fe1d3
josias.soares <josias.mota.soares@gmail.com>
2020-10-04 18:59:31 -0300

    list tasks in recyclerview

--- app/src/main/java/com/example/tasks/view/viewholder/TaskViewHolder.kt
+++ app/src/main/java/com/example/tasks/view/viewholder/TaskViewHolder.kt
@@ -1,18 +1,22 @@
 package com.example.tasks.view.viewholder
 
 import android.app.AlertDialog
+import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tasks.R
 import com.example.tasks.service.listener.TaskListener
 import com.example.tasks.service.model.TaskModel
 import com.example.tasks.service.repository.PriorityRepository
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TaskViewHolder(itemView: View, val listener: TaskListener) :
     RecyclerView.ViewHolder(itemView) {
+
+    private val mDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
 
     private val mPriorityRepository = PriorityRepository(itemView.context)
 
@@ -24,38 +28,38 @@ class TaskViewHolder(itemView: View, val listener: TaskListener) :
     /**
      * Atribui valores aos elementos de interface e também eventos
      */
-    fun bindData(taskModel: TaskModel) {
+    fun bindData(task: TaskModel) {
 
-        this.mTextDescription.text = taskModel.description
-        this.mTextPriority.text = mPriorityRepository.get(taskModel.priorityId).description
-        this.mTextDueDate.text = taskModel.dueDate
+        this.mTextDescription.text = task.description
+        this.mTextPriority.text = mPriorityRepository.get(task.priorityId).description
 
-        if (taskModel.complete) {
-            this.mImageTask.setImageDrawable(
-                ContextCompat.getDrawable(
-                    itemView.context,
-                    R.drawable.ic_done
-                )
-            )
+        val date = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).parse(task.dueDate)
+        this.mTextDueDate.text = mDateFormat.format(date)
+
+        if (task.complete) {
+            mTextDescription.setTextColor(Color.GRAY)
+            this.mImageTask.setImageResource(R.drawable.ic_done)
         } else {
-            this.mImageTask.setImageDrawable(
-                ContextCompat.getDrawable(
-                    itemView.context,
-                    R.drawable.ic_todo
-                )
-            )
+            mTextDescription.setTextColor(Color.BLACK)
+            this.mImageTask.setImageResource(R.drawable.ic_todo)
         }
 
         // Eventos
-        // mTextDescription.setOnClickListener { listener.onListClick(task.id) }
-        // mImageTask.setOnClickListener { }
+        mTextDescription.setOnClickListener { listener.onListClick(task.id) }
+        mImageTask.setOnClickListener {
+            if (task.complete){
+                listener.onUndoClick(task.id)
+            } else {
+                listener.onCompleteClick(task.id)
+            }
+        }
 
         mTextDescription.setOnLongClickListener {
             AlertDialog.Builder(itemView.context)
                 .setTitle(R.string.remocao_de_tarefa)
                 .setMessage(R.string.remover_tarefa)
                 .setPositiveButton(R.string.sim) { dialog, which ->
-                    // listener.onDeleteClick(task.id)
+                    listener.onDeleteClick(task.id)
                 }
                 .setNeutralButton(R.string.cancelar, null)
                 .show()
